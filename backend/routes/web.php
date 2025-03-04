53bfe35fd6b106a82a8017b9572f17e9d7f47255
Alisson S <alissonsandes@hotmail.com>
2025-03-04 19:17:57 -0300

    add api routes for register and login

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,10 +1,4 @@
 <?php
 
-use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // return '<img style = "width:100%"src="https://cdna.artstation.com/p/assets/images/images/034/590/744/original/sigi-daly-sonic-running-larger.gif?1612710639" />';
-    // reuturn view welcome
-    return Response()->view('welcome');
-});
